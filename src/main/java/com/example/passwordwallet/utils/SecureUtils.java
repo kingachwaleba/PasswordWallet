e841280008ed1b4c04c2d988c5a6a0191fb56371
kingachwaleba <kingach6@gmail.com>
2022-10-14 22:20:27 +0200

    Add comment

--- src/main/java/com/example/passwordwallet/utils/SecureUtils.java
+++ src/main/java/com/example/passwordwallet/utils/SecureUtils.java
@@ -36,6 +36,10 @@ public class SecureUtils {
     }
 
     public static String getSalt() throws NoSuchAlgorithmException {
+        // The name of the pseudo-random number generation (PRNG) algorithm supplied by the SUN provider.
+        // This algorithm uses SHA-1 as the foundation of the PRNG.
+        // It computes the SHA-1 hash over a true-random seed value concatenated with a 64-bit counter
+        // which is incremented by 1 for each operation. From the 160-bit SHA-1 output, only 64 bits are used.
         SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
         byte[] salt = new byte[16];
         random.nextBytes(salt);
